e7a81039e14055da263e108fe389e601fc1847ef
Horllydrey <horlly582@gmail.com>
2022-09-12 08:58:55 -0700

    Variable

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+ * main - Entry point
+ * Description: prints all single numbers of base 10 from 0
+ * Return: Always 0 (successful)
+ */
+int main(void)
+{
+	int y;
+
+	for (y = 0; y <= 10; y++)
+	{
+		putchar("%d", a);
+	}
+	puts('\n');
+	return (0);
+}
