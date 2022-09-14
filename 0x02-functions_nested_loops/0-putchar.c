e652b49290a3de5bfdad8ba981978677a0b4a4c8
Horllydrey <horlly582@gmail.com>
2022-09-14 07:43:14 -0700

    Loop

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Description: print _putchar followed by a new line
+ * The program should return 0
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar(112);
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
