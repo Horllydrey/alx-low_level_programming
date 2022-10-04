9d654af5c948ee868a276d55103b55322495e242
Horllydrey <horlly582@gmail.com>
2022-10-04 13:32:27 -0700

    lastt

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,8 +11,10 @@ int print_last_digit(int n)
 	int x;
 
 	if (n < 0)
-		n = -n;
-	x = n % 10;
-	_putchar(x + '0'
-	return (x);
+		n = -1 *(r % 10);
+	else
+		x = n % 10;
+
+	_putchar((x % 10) + '0');
+	return (x % 10);
 }
