06d47fcd8a3d2be72f113ebd8bb24d65cfbeebd0
Horllydrey <horlly582@gmail.com>
2022-10-06 05:16:09 -0700

    2arg

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,39 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
 /**
- * main - prints minimum number of coins to make change for an amount of money.
- * @argc: number of arguments passed to the function
- * @argv: argument vector of pointers to strings
- * Return: 0 if no errors, else 1
+ * main - Prints all arguments passed into main
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
-int a, n = 0, i, t;
-int c[5] = {25, 10, 5, 2, 1};
+	int i;
 
-if (argc != 2)
-{
-puts("Error");
-return (1);
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
 }
-a = atoi(argv[1]);
-if (a <= 0)
-{
-puts("0");
-return (1);
-}
-else
-{
-for (i = 0; i < 5; i++)
-{
-t = a / c[i];
-a -= t  *c[i];
-n += t;
-if (a == 0)
-break;
-}
-}
-printf("%d\n", n);
-return (0);
