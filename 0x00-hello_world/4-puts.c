cc0033c545eab34253caa8befe9bd482f6c47ab9
Oluwadare Titus olamide <horlly582@gmail.com>
2024-07-23 06:08:31 -0700

    put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: C program that prints exactly "Programming is like building a multilingual puzzle", followed by a new line.
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
