98f297a76a1144bee1e857812f02cf20877f05e0
Oluwadare Titus olamide <horlly582@gmail.com>
2024-07-24 07:12:18 -0700

    char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/** main - entry point
+ * Description: program that prints single integer
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int num = '0';
+	
+	while(num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	{
+	putchar('\n');
+	}
+	return (0);
+}
