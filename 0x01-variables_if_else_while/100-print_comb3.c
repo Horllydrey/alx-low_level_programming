528033e3a1e1145d39c78a1392db9d59d39ed32b
Oluwadare Titus olamide <horlly582@gmail.com>
2024-07-25 03:31:14 -0700

    comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: function that combines integers
+ * Return: o if successful
+ */
+int main(void)
+{
+	int a = '0';
+	int b = '0';
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; b <= '9'; b++)
+		{
+
+			if (!(( a == b  ) || (a > b)))
+			{
+				putchar(a);
+				putchar(b);
+				if (!((a == '9') && ( b == '8')))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
