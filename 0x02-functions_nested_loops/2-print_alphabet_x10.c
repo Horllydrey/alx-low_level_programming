d2fda73e2251a9282a946547ce14264d12bf4f25
Oluwadare Titus olamide <horlly582@gmail.com>
2024-07-27 15:57:43 -0700

    gp

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,16 +2,16 @@
 
 /**
  * print_alphabet_x10 - print x10
- * 
  * return:void
  */
 void print_alphabet_x10(void)
 {
 	int ch;
 	int i;
+
 	i = 0;
-	 
-	while(i < 10)
+
+	while (i < 10)
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
 		{
