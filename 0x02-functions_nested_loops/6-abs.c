65c430bb3935792b2d799466c28004dc3a056f91
Oluwadare Titus olamide <horlly582@gmail.com>
2024-08-02 06:47:09 -0700

    ttt

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "main.h"
 /**
  * _abs - a function that prints the absolute value of a number
@@ -17,25 +16,3 @@ int _abs(int number)
 		return (number * -1);
 	}
 }
-
-=======
-#include "main.h"
-/**
- * _abs - a function that prints the absolute value of a number
- * @number: parameter to be checked
- * Return: 0 if successful
- */
-int _abs(int number)
-{
-
-	if (number > 0 || number == 0)
-	{
-		return (number);
-	}
-	else
-	{
-		return (number * -1);
-	}
-}	
-
->>>>>>> 513e782f43c732338ed427ca3d120063ffff3264
