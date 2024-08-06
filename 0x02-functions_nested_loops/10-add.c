395c6e8379c7337cc57dfcb407fd61dcb8687aff
Oluwadare Titus olamide <horlly582@gmail.com>
2024-08-06 07:17:21 -0700

    ff

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * add - it adds two integer
+ * Description: It adds two integer and prints the result
+ * Return: 0 if successful
+ */
+int add(int num1, int num2)
+{
+	 return(num1 + num2);
+}
