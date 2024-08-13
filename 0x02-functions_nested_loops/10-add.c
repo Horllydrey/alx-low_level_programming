a7dce956a6e64cd8b14137935fcff77122647d10
Oluwadare Titus olamide <horlly582@gmail.com>
2024-08-13 03:25:07 -0700

    ff

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,8 +2,8 @@
 /**
  * add - it adds two integer
  * Description: It adds two integer and prints the result
- * num1: a parameter to be checked
- * num2: second parameter
+ * @num1: a parameter to be checked
+ * @num2: second parameter
  * Return: 0 if successful
  */
 int add(int num1, int num2)
